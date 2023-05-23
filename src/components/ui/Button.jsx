02e403d1efa445ae54567e78d5d2e6fcd039bc26
hheeseung <ssigu69@gmail.com>
2023-05-23 17:59:53 +0900

    feat: 상품 상세화면

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,7 +1,7 @@
 export default function Button({ text, onClick, disabled }) {
   return (
     <button
-      className="bg-brand text-white py-2 px-4 rounded-md hover:brightness-110"
+      className="w-full bg-brand text-white py-2 px-4 rounded-md hover:brightness-110"
       onClick={onClick}
       disabled={disabled}
     >
